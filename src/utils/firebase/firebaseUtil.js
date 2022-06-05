0123bd91eaa78af189b25b1137de33e79aa7900d
Brzeziu22 <kamilb79@gmail.com>
2022-06-05 17:55:36 +0200

    User doc stored in firestore

--- src/utils/firebase/firebaseUtil.js
+++ src/utils/firebase/firebaseUtil.js
@@ -1,5 +1,7 @@
 import {initializeApp} from 'firebase/app';
 import {getAuth,signInWithRedirect, signInWithPopup,GoogleAuthProvider} from 'firebase/auth'
+import {getFirestore,doc,getDoc,setDoc} from 'firebase/firestore'
+
 const firebaseConfig = {
     apiKey: "AIzaSyCcTZNllPDNxe4vgwJLctyMBJ4sN_2Ec1g",
     authDomain: "eshopdb-54f11.firebaseapp.com",
@@ -8,6 +10,7 @@ const firebaseConfig = {
     messagingSenderId: "1098891930322",
     appId: "1:1098891930322:web:f655432cdb5cca14bedb0b"
   };
+
 
 
   const firebaseApp = initializeApp(firebaseConfig);
@@ -19,3 +22,27 @@ const firebaseConfig = {
 
   export const auth = getAuth();
   export const signInWithGooglePopup = ()=> signInWithPopup(auth,provider)
+
+  export const db =getFirestore();
+
+  export const createUserDocumentFromAuth=async (userAuth)=>{
+    const userDocRef=doc(db,'users',userAuth.uid);
+    const userSnapshot= await getDoc(userDocRef);
+    console.log(userSnapshot.exists());
+
+    if(!userSnapshot.exists()){
+        const {displayName,email}=userAuth;
+        const createdAt =new Date();
+        try{
+            await setDoc(userDocRef,{
+                displayName,email,createdAt
+            });
+        }catch (error){
+            console.log('error creating the user', error.massage);
+        }
+    }
+    return userDocRef;
+    //if user data exist 
+    //if user data do not exist{setDoc with user data }
+    //if user data exist return userdoc
+  }
