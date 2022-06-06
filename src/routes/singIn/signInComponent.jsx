6e666e9a8d5f778e16acbb48888d248304020807
Brzeziu22 <kamilb79@gmail.com>
2022-06-06 20:01:54 +0200

    sign up form with change handle storing values in state

--- src/routes/singIn/signInComponent.jsx
+++ src/routes/singIn/signInComponent.jsx
@@ -1,22 +1,18 @@
 import {signInWithGooglePopup,createUserDocumentFromAuth,auth} from '../../utils/firebase/firebaseUtil'
-
+import SignUpForm from '../../components/singUpForm/signUpFormComponent';
 
 const SignIn = () => {
-    
-
-
-
     const logGoogleUser = async ()=>{
         const {user} =await signInWithGooglePopup();
         const userDocRef=await createUserDocumentFromAuth(user)
-    }
+    };
    
 
-    return ( 
-    
+    return (
         <div>
             Logowanie
             <button onClick={logGoogleUser}>Sign in with Google Popup</button>
+            <SignUpForm/>
         </div>
      );
 }
