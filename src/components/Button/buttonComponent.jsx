be30d042905fe9e76a164a2a71449fa0f3f9b8d2
Brzeziu22 <kamilb79@gmail.com>
2022-07-03 17:44:59 +0200

    all styled components work fine

--- src/components/Button/buttonComponent.jsx
+++ src/components/Button/buttonComponent.jsx
@@ -1,13 +1,21 @@
 import './buttonStyle.scss'
-
+import { BaseButton,GoogleSignInButton,InvertedButton } from './buttonStyle';
 const buttonTypeClasses={
-    google:"googleSignIn",
+    base:`base`,
+    google:"google",
     inverted:"inverted"
 }
 
-const Button = ({children,buttonType, ...otherprops}) => {
+const getButton = (buttonType = buttonTypeClasses.base) =>
+  ({
+    [buttonTypeClasses.base]: BaseButton,
+    [buttonTypeClasses.google]: GoogleSignInButton,
+    [buttonTypeClasses.inverted]: InvertedButton,
+  }[buttonType]);
 
-    return ( <button className={`buttonContainer ${buttonTypeClasses[buttonType]}`} {...otherprops}>{children}</button> );
+const Button = ({children,buttonType, ...otherprops}) => {
+    const CurrentButton=getButton(buttonType)
+    return ( <CurrentButton{...otherprops}>{children}</CurrentButton> );
 }
  
-export default Button;
+export default Button; 
